ef36fa6954626b869554c17cdb86779bc6796237
ousakka <oumaima.sakka.os@gmail.com>
2025-04-19 20:21:50 +0200

    Creating objects in array

--- exercices/exercice 1/script.js
+++ exercices/exercice 1/script.js
@@ -6,3 +6,28 @@ arr[(arr.length)-1]="dog";
 
 console.log(arr)
 // ["lion", "tiger", "bear", "wolf", "dog"]
+
+
+//square all exercice
+let numbers = [5, 6, 2, 0, 1, 9]
+
+// square all the values
+
+/* let numbers2= numbers.map((n)=>n*n);
+numbers2.forEach(n => {
+    console.log(n);
+  }); */
+
+//  Filter the elements from numbers3 that **cannot be divided by 2**
+  let numbers3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function dividedByTwo(n) {
+  return (n%2);
+}
+
+let numbers3Filter = numbers3.filter(n=>!dividedByTwo(n));
+
+console.log(numbers3Filter);
+
+
+
